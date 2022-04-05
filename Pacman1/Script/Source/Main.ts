31337b3c9aae7da6a3faf34a5d93775b137805ce
Xanderthier <Thier32@gmx.de>
2022-04-05 15:58:11 +0200

    Pacman update

---
+++ Pacman1/Script/Source/Main.ts
@@ -0,0 +1,48 @@
+namespace Script {
+  import ƒ = FudgeCore;
+  ƒ.Debug.info("Main Program Template running!");
+
+  let viewport: ƒ.Viewport;
+  let pacman: ƒ.Node;
+  let direction: ƒ.Vector3 = ƒ.Vector3.ZERO();
+  let speed: number = 1 / 60;
+  document.addEventListener("interactiveViewportStarted", <EventListener>start);
+
+  function start(_event: CustomEvent): void {
+    viewport = _event.detail;
+    let graph: ƒ.Node = viewport.getBranch();
+    pacman = graph.getChildrenByName("Pacman")[0];
+    console.log(pacman);
+
+
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+  }
+
+  function update(_event: Event): void {
+    // ƒ.Physics.simulate();  // if physics is included and used
+    let posPacman: ƒ.Vector3 = pacman.mtxLocal.translation;
+    let nearestGridPoint: ƒ.Vector2 = new ƒ.Vector2(Math.round(posPacman.x), Math.round(posPacman.y));
+    let nearGridPoint: boolean = posPacman.toVector2().equals(nearestGridPoint, 2 * speed);
+
+    if (nearGridPoint) {
+      let directionOld: ƒ.Vector3 = direction.clone;
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+        direction.set(1, 0, 0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
+        direction.set(-1, 0, 0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]))
+        direction.set(0, 1, 0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]))
+        direction.set(0, -1, 0);
+
+      if (!direction.equals(directionOld))
+        pacman.mtxLocal.translation = nearestGridPoint.toVector3();
+    }
+
+    pacman.mtxLocal.translate(ƒ.Vector3.SCALE(direction, speed));
+    viewport.draw();
+    ƒ.AudioManager.default.update();
+  }
+}
